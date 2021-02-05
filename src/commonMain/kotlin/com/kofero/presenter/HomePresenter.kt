9ffd0d9ddbb93c4590f7f34478c87780c984cc00
mitchdrew <mitchdrew@goatse.cx>
2021-02-05 00:12:08 -0800

    master - mdrew: fixed model importing

---
+++ src/commonMain/kotlin/com/kofero/presenter/HomePresenter.kt
@@ -0,0 +1,7 @@
+package com.kofero.presenter
+
+import com.kofero.model.Character
+
+class HomePresenter(val char:Character): IHomePresenter {
+
+}
---
+++ src/commonMain/kotlin/com/kofero/presenter/IHomePresenter.kt
@@ -0,0 +1,4 @@
+package com.kofero.presenter
+
+interface IHomePresenter {
+}
