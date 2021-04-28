0228a222dd37cd79b4c43fab78c7e40c6407c571
mitchdrew <mitchdrew@goatse.cx>
2021-04-28 11:23:36 -0700

    master - mdrew: GamePresenter work

--- src/commonMain/kotlin/ro/kofe/presenter/game/GamePresenter.kt
+++ src/commonMain/kotlin/ro/kofe/presenter/game/GamePresenter.kt
@@ -2,36 +2,67 @@ package ro.kofe.presenter.game
 
 import ro.kofe.model.Character
 import ro.kofe.model.Game
+import ro.kofe.presenter.IFreezer
 import ro.kofe.presenter.IImageProvider
 import ro.kofe.presenter.IProvider
 import ro.kofe.presenter.IProviderListener
 
-class GamePresenter(private val characterProvider: IProvider<Character>, private val gameProvider: IProvider<Game>, private val imageProvider:IImageProvider): IGamePresenter {
+class GamePresenter(private var freezer: IFreezer?, private var characterProvider: IProvider<Character>?, private var gameProvider: IProvider<Game>?, private var imageProvider:IImageProvider?): IGamePresenter {
     private var view:IGameView? = null
     private var gameListener:IProviderListener<Game>? = null
     private var characterListener:IProviderListener<Character>? = null
     private var imageListener:IImageProvider.Listener? = null
 
 
+    private fun getGameListener(): IProviderListener<Game>{
+        return object : IProviderListener<Game> {
+            override fun onReceive(ids: List<Int>, elements: List<Game>) {
+                freezer?.freeze(elements)
+                view?.display(elements[0])
+                characterProvider?.get(elements[0].characterIds)
+            }
+            override fun onError(ids: List<Int>, error: Exception) {
+                view?.error(error)
+            }
+        }
+    }
+
     private fun getCharacterListener(): IProviderListener<Character> {
         return object : IProviderListener<Character>{
-            override fun onReceive(ids: List<Int>, elements: List<Character>) {}
-            override fun onError(ids: List<Int>, error: Exception) {}
+            override fun onReceive(ids: List<Int>, elements: List<Character>) {
+                freezer?.freeze(elements)
+                view?.display(elements)
+                for(char in elements){
+                    imageProvider?.get(char.iconUrl)
+                }
+            }
+            override fun onError(ids: List<Int>, error: Exception) {
+                view?.error(error)
+            }
         }
     }
 
     private fun getImageListener(): IImageProvider.Listener {
         return object : IImageProvider.Listener {
-            override fun onReceive(url: String, imgBase64: String) {}
-            override fun onError(url: String, error: Exception) {}
+            override fun onReceive(url: String, imgBase64: String) {
+                view?.display(url,imgBase64)
+            }
+            override fun onError(url: String, error: Exception) {
+                view?.error(error)
+            }
         }
     }
 
     private fun attachListeners(){
         characterListener = getCharacterListener()
         imageListener = getImageListener()
-        characterProvider.addListener(characterListener!!)
-        imageProvider.addListener(imageListener!!)
+        gameListener = getGameListener()
+        freezer?.freeze(characterListener!!)
+        freezer?.freeze(gameListener!!)
+        freezer?.freeze(imageListener!!)
+        gameProvider?.addListener(gameListener!!)
+        characterProvider?.addListener(characterListener!!)
+        imageProvider?.addListener(imageListener!!)
     }
 
     override fun setView(view: IGameView) {
@@ -40,8 +71,18 @@ class GamePresenter(private val characterProvider: IProvider<Character>, private
     }
 
     override fun showGame(id: Int) {
+        var list = ArrayList<Int>()
+        list.add(id)
+        gameProvider?.get(list)
     }
 
     override fun shutdown() {
+        gameListener?.let{gameProvider?.removeListener(it)}
+        imageListener?.let{imageProvider?.removeListener(it)}
+        characterListener?.let{characterProvider?.removeListener(it)}
+        view = null
+        gameProvider = null
+        imageProvider = null
+        characterProvider = null
     }
 }
--- src/commonMain/kotlin/ro/kofe/presenter/game/IGameView.kt
+++ src/commonMain/kotlin/ro/kofe/presenter/game/IGameView.kt
@@ -6,5 +6,6 @@ import ro.kofe.model.Game
 interface IGameView {
     fun display(game:Game)
     fun display(characters:List<Character>)
+    fun display(url:String, imgBase64:String)
     fun error(error:Exception)
 }
